6eeba4b8fd8d783e0dd04790b727d1ff87709b98
Yiyu Ni <niyiyu@uw.edu>
2023-02-28 01:18:42 -0800

    add support storing CCF to S3 in tiledb

--- src/S1_preprocess_correlate.py
+++ src/S1_preprocess_correlate.py
@@ -3,12 +3,12 @@ sys.path.append("../DASStore")
 
 import os
 import time
-import pyasdf
 import numpy as np
 import DAS_module
 from mpi4py import MPI
 from datetime import datetime, timedelta
 from dasstore.zarr import Client
+import tiledb
 
 if not sys.warnoptions:
     import warnings
@@ -34,8 +34,20 @@ TODO:
 #######################################################
 tt0=time.time()
 
-# data/file paths
-CCFDIR      = "/home/niyiyu/Research/SeaDASCorr/DASStore_CCF"
+# pnwstore1_endpoint = "pnwstore1.ess.washington.edu:9000"
+
+config = tiledb.Config()
+config["vfs.s3.scheme"] = "https"
+config["vfs.s3.region"] = "us-west-2"
+config["vfs.s3.endpoint_override"] = ""
+config["vfs.s3.use_virtual_addressing"] = "true"
+ctx = tiledb.Ctx(config)
+ccf_bucket = "seadas-december-2022-ccf"
+
+bucket = "seadas-december-2022"
+endpoint = "s3.us-west-2.amazonaws.com"
+secure = True
+role_assigned = True
 
 # useful parameters for preprocessing the data
 input_fmt   = 'zarr'                                                      # input file format between 'sac' and 'mseed' 
@@ -71,12 +83,8 @@ nsta = len(cha_list)
 # worker info
 ARRAY_SIZE = int(os.environ['AWS_BATCH_JOB_ARRAY_SIZE'])
 ARRAY_INDEX = int(os.environ['AWS_BATCH_JOB_ARRAY_INDEX'])
-
-# client information
-bucket = "seadas-december-2022"
-endpoint = "s3.us-west-2.amazonaws.com"
-secure = True
-role_assigned = True
+# ARRAY_SIZE = 1
+# ARRAY_INDEX = 0
 
 #---------MPI-----------
 comm = MPI.COMM_WORLD
@@ -90,22 +98,26 @@ size = comm.Get_size()
 # prepare client and time range
 client = Client(bucket, endpoint, secure = secure, role_assigned = role_assigned)
 if rank == 0:
-    t0 = datetime.fromisoformat(client.meta['acquisition.acquisition_start_time']).date()
-    t1 = datetime.fromisoformat(client.meta['acquisition.acquisition_end_time']).date()
-    n_days = (t1 - t0 + timedelta(days=1)).days
-    date_list = [t0 + timedelta(days = i) for i in range(n_days)]
+    acq_t0 = datetime.fromisoformat(client.meta['acquisition.acquisition_start_time'])
+    acq_t1 = datetime.fromisoformat(client.meta['acquisition.acquisition_end_time'])
+    n_minute = ((acq_t1 - acq_t0).days + 1) * 1440
+    # n_days = (t1 - t0 + timedelta(days=1)).days
+    # minute_list = [t0 + timedelta(days = i) for i in range(n_days)]
 else:
-    date_list = []
+    # date_list = []
+    acq_t0 = 0
+    n_minute = 0
 
 # split jobs by rank
-date_list   = comm.bcast(date_list,root=0)
-array_split = np.array_split(date_list, ARRAY_SIZE)[ARRAY_INDEX] 
-im = np.arange(1440)
-rank_split = np.array_split(im, size)[rank]                                         
+# date_list   = comm.bcast(date_list,root=0)
+n_minute = comm.bcast(n_minute,root=0)
+acq_t0 = datetime.fromtimestamp(comm.bcast(acq_t0.timestamp(),root=0))
+array_split = np.array_split(np.arange(n_minute), ARRAY_SIZE)[ARRAY_INDEX] 
+rank_split = np.array_split(array_split, size)[rank]                                         
 
 ##################################################
 # assemble parameters for data pre-processing
-prepro_para = {'CCFDIR':CCFDIR,
+prepro_para = {'CCF_BUCKET':ccf_bucket,
                'input_fmt':input_fmt,
                'freqmin':freqmin,
                'freqmax':freqmax,
@@ -122,20 +134,16 @@ prepro_para = {'CCFDIR':CCFDIR,
                'maxlag':maxlag,
                'max_over_std':max_over_std,
                'MAX_MEM':MAX_MEM}
-metadata = os.path.join(CCFDIR,'prepro_fft_info.txt') 
+# metadata = os.path.join(CCFDIR,'prepro_fft_info.txt') 
 
 ##########################################################
 #################PROCESSING SECTION#######################
 ##########################################################
 
 if rank == 0:
-#     # make directory
-    if not os.path.isdir(CCFDIR):
-        os.mkdir(CCFDIR)
-
     # output parameter info
-    fout = open(metadata,'w')
-    fout.write(str(prepro_para));fout.close()
+    # fout = open(metadata,'w')
+    # fout.write(str(prepro_para));fout.close()
 
     # rough estimation on memory needs needed in S1 (assume float32 dtype)
     memory_size = nsta*npts_chunk*4/1024**3
@@ -143,93 +151,68 @@ if rank == 0:
         raise ValueError('Require %5.3fG memory but only %5.3fG provided)! Reduce inc_hours to avoid this issue!' % (memory_size,MAX_MEM))
 
 # MPI: loop through each time-chunk
-for i in array_split:
-    # one job
-    job_t0 = datetime(year = i.year, month = i.month, day = i.day, 
-                       hour = 0, minute = 0, second = 0, microsecond=0)
-    
-    for ii in rank_split:
-        t0=time.time()
+for ii in rank_split:
+    t0=time.time()
 
-        starttime = job_t0 + timedelta(minutes = int(ii))
-        endtime   = job_t0 + timedelta(minutes = int(ii+1))
-        tdata = client.get_data(cha_list, starttime, endtime).T
-        print(starttime)
+    starttime = acq_t0 + timedelta(minutes = int(ii))
+    endtime   = acq_t0 + timedelta(minutes = int(ii+1))
+    tdata = client.get_data(cha_list, starttime, endtime).T
+    print(starttime)
 
-        # check if data has NaN
-        if not np.isnan(tdata).any():
+    # check if data has NaN
+    if not np.isnan(tdata).any():
 
-            # perform pre-processing
-            trace_stdS,dataS = DAS_module.preprocess_raw_make_stat(tdata,prepro_para)
-            t1 = time.time()
-            if flag:
-                print('pre-processing & make stat takes %6.2fs'%(t1-t0))
+        # perform pre-processing
+        trace_stdS,dataS = DAS_module.preprocess_raw_make_stat(tdata,prepro_para)
+        t1 = time.time()
+        if flag:
+            print('pre-processing & make stat takes %6.2fs'%(t1-t0))
 
-            # do normalization if needed
-            white_spect = DAS_module.noise_processing(dataS,prepro_para)
-            Nfft = white_spect.shape[1];Nfft2 = Nfft//2
+        # do normalization if needed
+        white_spect = DAS_module.noise_processing(dataS,prepro_para)
+        Nfft = white_spect.shape[1];Nfft2 = Nfft//2
 
-            # load fft data in memory for cross-correlations
-            data = white_spect[:,:Nfft2]
-            del dataS,white_spect
-            
-            # remove channel of potential local traffic noise
-            ind = np.where((trace_stdS<prepro_para['max_over_std']) &
-                            (trace_stdS>0) &
-                            (np.isnan(trace_stdS)==0))[0]
-            if not len(ind):
-                raise ValueError('the max_over_std criteria is too high which results in no data')
-            sta = cha_list[ind]
-            white_spect = data[ind]
+        # load fft data in memory for cross-correlations
+        data = white_spect[:,:Nfft2]
+        del dataS,white_spect
+        
+        # remove channel of potential local traffic noise
+        ind = np.where((trace_stdS<prepro_para['max_over_std']) &
+                        (trace_stdS>0) &
+                        (np.isnan(trace_stdS)==0))[0]
+        if not len(ind):
+            raise ValueError('the max_over_std criteria is too high which results in no data')
+        sta = cha_list[ind]
+        white_spect = data[ind]
 
-            # do cross-correlation now
-            temp = starttime.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-4]+'T'+ endtime.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-4]
-            tmpfile = os.path.join(CCFDIR,temp+'.tmp')
-            ftmp = open(tmpfile,'w')
-            t2 = time.time()
-            if flag:
-                print('it takes %6.2fs before getting into the cross correlation'%(t2-t1))
+        # do cross-correlation now
+        temp = starttime.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-4]+'T'+ endtime.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-4]
+        t2 = time.time()
+        if flag:
+            print('it takes %6.2fs before getting into the cross correlation'%(t2-t1))
 
-            # loop through each source
-            outname = os.path.join(CCFDIR,temp+'.h5')
-            cc_h5 = os.path.join(CCFDIR,outname)
-            for iiS in range(len(sta)-1):
-            # for iiS in range(10):
+        with tiledb.open(f"s3://{ccf_bucket}/", 'w', ctx = ctx) as A:
+            # for iiS in range(len(sta)-1):
+            for iiS in range(1):
                 if flag:
                     print('working on source %s'%sta[iiS])
 
                 # smooth the source spectrum
                 sfft1 = DAS_module.smooth_source_spect(white_spect[iiS],prepro_para)
-                corr,tindx = DAS_module.correlate(sfft1,white_spect[iiS+1:],prepro_para,Nfft)
+                corr,tindx = DAS_module.correlate(sfft1, white_spect[iiS+1:],prepro_para,Nfft)
 
                 # update the receiver list
                 tsta = sta[iiS+1:]
                 receiver_lst = sta[tindx]
 
-                # save cross-correlation into ASDF file
-                for iiR in range(len(receiver_lst)):
-                    with pyasdf.ASDFDataSet(cc_h5,mpi=False) as ccf_ds:
-                        # use the channel number as a way to figure out distance
-                        Rindx = np.where(sta==receiver_lst[iiR])[0]
-                        Sindx = iiS
-                        param = {'dist':int(Rindx-Sindx)*gaug_len,
-                                'sps':samp_freq,
-                                'dt': 1/samp_freq,
-                                'maxlag':maxlag,
-                                'freqmin':freqmin,
-                                'freqmax':freqmax}
-                    
-                        # source-receiver pair
-                        data_type = sta[iiS]
-                        path = str(sta[iiS])+'_'+str(receiver_lst[iiR])
-                        ccf_ds.add_auxiliary_data(data=corr[iiR], 
-                                                data_type=str(data_type), 
-                                                path=path, 
-                                                parameters=param)
-                        ftmp.write(str(sta[iiS])+'_'+str(receiver_lst[iiR])+'\n')
-        
-        t3=time.time()
-        print('it takes '+str(t3-t2)+' s to cross correlate one chunk of data')
+                corr_full = np.zeros([600, 1001]) * np.nan
+                corr_full[tindx, :] = corr
+
+                iiiS = sta[iiS] - 500
+                A[:, iiiS*600:(iiiS+1)*600, ii] = corr_full.T
+    
+    t3=time.time()
+    print('it takes '+str(t3-t2)+' s to cross correlate one chunk of data')
 
 tt1=time.time()
 print('step0B takes '+str(tt1-tt0)+' s')
